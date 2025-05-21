1addfc8cc3deb1733cdd6b1f0e65006acede9102
Fauzan Hafizh <119883529+Fruzh@users.noreply.github.com>
2025-05-21 21:08:09 +0700

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -28,6 +28,17 @@ function reveal() {
 }
 
 window.addEventListener("scroll", reveal);
+
+const birthDate = new Date('2008-05-31'); // format YYYY-MM-DD
+const today = new Date();
+
+let age = today.getFullYear() - birthDate.getFullYear();
+const m = today.getMonth() - birthDate.getMonth();
+if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+    age--;
+}
+
+document.getElementById('age').textContent = age;
 // ===== Vanila Javascript (END)
 
 
